8849a6e2c6c7a58bf0a9c06c9aba9a6d2b7ab02a
msciborski <michal.sciborski@student.put.poznan.pl>
2019-02-12 22:02:20 +0100

    initial commit, basic config of react, webpack, chrome extension hot reloading

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ChromeExtensionReloader  = require('webpack-chrome-extension-reloader');
+
+module.exports = {
+  mode: 'development',
+  watch: 'true',
+  entry: {
+    background: './src/background.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new ChromeExtensionReloader({
+      port: 9090, // Which port use to create the server
+      reloadPage: true, // Force the reload of the page also
+      entries: { // The entries used for the content/background scripts
+        background: 'background' // *REQUIRED
+      }
+    })
+  ]
+};
